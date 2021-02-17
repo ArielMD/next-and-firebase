1290896ed5ddacd340b4ae727d20e81efed975a3
Ariel May <aemd199711@gmail.com>
2021-02-16 22:18:35 -0600

    feat: html para el formulario del login

---
+++ pages/home.tsx
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const Home: React.FC = () => {
+  return <Layout title="Home">Hello</Layout>;
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,52 @@
+import { useForm } from "react-hook-form";
 import Layout from "../components/Layout";
 
-const Home: React.FC = () => {
-  return <Layout title="Home">Hello</Layout>;
+interface IFormLogin {
+  user: String;
+  password: String;
+}
+
+const Login: React.FC = () => {
+  const { register, handleSubmit, errors } = useForm<IFormLogin>();
+
+  const onSubmit = (data: IFormLogin) => {
+    console.log(data);
+  };
+
+  return (
+    <Layout title="Login">
+      <h1>LOGIN</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="">Usuario</label>
+          <input
+            type="text"
+            name="user"
+            ref={register({
+              required: "El campo es requerido",
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "El campo no puede contener numeros",
+              },
+            })}
+          />
+          {errors.user && <small>{(errors.user as any)?.message}</small>}
+        </div>
+        <div>
+          <label htmlFor="">Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            ref={register({ required: "El campo es requerido" })}
+          />
+          {errors.password && (
+            <small>{(errors.password as any)?.message}</small>
+          )}
+        </div>
+        <button>Entrar</button>
+      </form>
+    </Layout>
+  );
 };
 
-export default Home;
+export default Login;
