5eb734c2ba2b05d21c60e7725dac1c77922c328f
Ariel May <aemd199711@gmail.com>
2021-02-17 16:23:53 -0600

    feat: Metodos para registrar y autentificar usuarios en Firebase

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -12,6 +12,21 @@ class Firebase {
 
     this.auth = app.auth();
   }
+
+  async registerUser(name: string, email: string, password: string) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+
+    return newUser.user.updateProfile({
+      displayName: name,
+    });
+  }
+
+  login(email: string, password: string) {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
 }
 
 export default new Firebase();
