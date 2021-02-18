8ec5dd80628b4d0363d358b81b1abeb1184edb38
Ariel May <aemd199711@gmail.com>
2021-02-17 18:30:21 -0600

    feat: redireccionar usuarios cuando existe una session activa en firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import firebase, { FirebaseContext } from "../firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsuscribe = firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+        router.push("/home");
+      } else {
+        setCurrentUser(null);
+        router.push("/");
+      }
+    });
+
+    return () => unsuscribe();
+  }, []);
   return (
     <FirebaseContext.Provider
       value={{
         firebase,
+        currentUser,
       }}
     >
       <Component {...pageProps} />
